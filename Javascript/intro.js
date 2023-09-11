32fb51bd285d15df7ebd1fbd74bf720a0d69e619
GogulaSivaGanesh <21A91A05D4@aec.edu.in>
2023-09-11 12:11:48 +0530

    commit

---
+++ Javascript/intro.js
@@ -0,0 +1,84 @@
+// // creating a varable using var keyword.
+// var a = 1;
+// document.writeln(a+"<br>");
+
+
+// document.writeln("hi this is javascript"+"<br>");
+
+
+// a = "hii";
+// document.write(a+"length of a "+a.length+"<br>")
+// //concating the string
+// a +=" hello"
+// // document.writeln("value of a is "+a);
+// document.write(a+" "+a.length+"<br>")
+
+
+// var name1 ="siva ";
+// document.write(name1+" "+name1.length+"<br>");
+// name1 +="pradeep";
+// document.write(name1+" "+name1.length+"<br>");
+// name1 +="&ram";
+// // document.write(name1);
+// document.write(name1+" "+name1.length+"<br>");
+
+
+// var n;
+// n = 10;
+// //type of variable
+// document.write(typeof(n));
+
+// var z = 'hi';
+// //printing the value by using console
+// console.log(z);
+
+
+// //alert box is used for display.
+// // there are window.alert and alert the short method is alert().
+// var x = 100;
+// alert(x);
+// window.alert(x);
+
+//boolean type
+// var q = true;
+// document.write(typeof(q));
+
+
+// //in javascript,an array can have multiple data types.
+// var arr = ['a',1,false,'c'];
+// // console.log is reprsents the all details of arr.
+// console.log(arr);
+// document.write(arr);
+
+// var t=[{
+//     'name':'siva',
+//     'Roll_no':'D4',
+//     'college':'Aec',
+//     'branch':'CSE',
+//     'email':'siva@gmail.com',
+// },{
+//     'name':'ganesh',
+//     'Roll_no':'21',
+//     'college':'Aec',
+//     'branch':'CSE',
+//     'email':'ganesh@gmail.com',
+// }]
+
+
+// for (let i = 0; i < t.length; i++) {
+//     console.log(t[i].name);
+//     console.log(t[i].Roll_no);
+//     console.log( t[i].college);
+//     console.log(t[i].branch);
+    
+// }
+
+
+// for(var i=0; i<t.length; i++){
+//     for(var key in t[i]){
+//         document.write(t[i][key]+' ');
+//     }
+//     document.write("<br>");
+// }
+
+
